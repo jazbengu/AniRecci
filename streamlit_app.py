386872e5c90c32d08bb77b84edee38c7b3e8f8be
Joy Azile Zoe Bengu <111288977+jazbengu@users.noreply.github.com>
2024-11-19 19:03:52 +0000

    INITIAL COMMIT OF NEW CODE

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import papermill as pm
+import pandas as pd
+import json
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def main():
+    st.title("AniRecci")
+    user_anime_titles = st.text_input("Enter your favorite anime titles separated by commas:")
+    
+    if st.button("Submit"):
+        # Split the titles and prepare them for passing
+        titles = user_anime_titles.split(',')
+
+        # Run the Jupyter notebook with parameters using papermill
+        pm.execute_notebook(
+            'anime_processing.ipynb',  # Path to your notebook
+            parameters={'user_anime_titles': json.dumps(titles)}  # Pass the titles as a JSON string
+        )
+
+        # Read the recommendations from the CSV file
+        recommendations_df = pd.read_csv('anime_recommendations.csv')
+        st.dataframe(recommendations_df)
+
+if __name__ == "__main__":
+    main()
